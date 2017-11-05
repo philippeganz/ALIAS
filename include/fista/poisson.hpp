858eb6122b21d00623b401a94bd7b070b2ae0c1a
Philippe Ganz <philippe.ganz@gmail.com>
2017-11-05 15:25:52 +0100

    Improved the FISTA solver for Poisson noise
    * Complete implementation of the FISTA algorithm
    * Reduce Poisson parameters to what is strictly needed

--- include/fista/poisson.hpp
+++ include/fista/poisson.hpp
@@ -3,7 +3,7 @@
 /// \brief FISTA (Fast Iterative Shrinkage Tresholding Algorithm) solver for Poisson distributed noise.
 /// \author Philippe Ganz <philippe.ganz@gmail.com> based on the work of Hatef Monajemi <monajemi@stanford.edu>
 /// \version 0.1.0
-/// \date 2017-07-13
+/// \date 2017-07-30
 /// \copyright GPL-3.0
 ///
 
@@ -12,6 +12,9 @@
 
 #include "datacontainer.hpp"
 #include "fista/poisson/parameters.hpp"
+
+#include <iostream>
+#include <iomanip>
 
 
 namespace astroqut{
@@ -23,16 +26,13 @@ namespace poisson{
  *  \param background Background shift
  *  \param response Response data to the regression matrix
  *  \param lambda Regularization parameter
- *  \param lambda_max
- *  \param lipschitz_const
  *  \param options Parameters that defines various value for FISTA to work
  */
-DataContainer<double> solve(  const DataContainer<double>& model,
-                              const DataContainer<double>& background,
-                              const DataContainer<double>& response,
-                              const double lambda,
-                              const double lambda_max,
-                              const Parameters& options );
+DataContainer<double> solve( const DataContainer<double>& model,
+                             const DataContainer<double>& background,
+                             const DataContainer<double>& response,
+                             const double lambda,
+                             const Parameters& options );
 
 } // namespace poisson
 } // namespace fista
--- include/fista/poisson/parameters.hpp
+++ include/fista/poisson/parameters.hpp
@@ -1,10 +1,10 @@
 ///
-/// \file include/fista/parameters.hpp
-/// \brief Parameters class for the Poisson distributed FISTA solver.
+/// \file include/fista/poisson/parameters.hpp
+/// \brief Parameters structure for the Poisson distributed FISTA solver.
 /// \details Allows the user to change the default parameters used by the solver.
 /// \author Philippe Ganz <philippe.ganz@gmail.com> based on the work of Hatef Monajemi <monajemi@stanford.edu>
 /// \version 0.1.0
-/// \date 2017-07-29
+/// \date 2017-08-13
 /// \copyright GPL-3.0
 ///
 
@@ -16,56 +16,21 @@
 namespace astroqut{
 namespace fista{
 namespace poisson{
-class Parameters
+struct Parameters
 {
-public:
-    /** Default constructor */
-    Parameters();
-    /** Default destructor */
-    virtual ~Parameters();
+    Parameters() noexcept
+        : tol(1e-6)
+        , max_iter(2000)
+        , init_value(DataContainer<double>())
+        , log(true)
+        , log_period(10)
+    {}
 
-    /** Access tol_
-     * \return The current value of tol_
-     */
-    float Tol() const noexcept { return tol_; }
-    /** Set tol_
-     * \param val New value to set
-     */
-    void Tol(float val) noexcept { tol_ = val; }
-    /** Access max_iter_
-     * \return The current value of max_iter_
-     */
-    size_t MaxIter() const noexcept { return max_iter_; }
-    /** Set max_iter_
-     * \param val New value to set
-     */
-    void MaxIter(size_t val) noexcept { max_iter_ = val; }
-    /** Access init_value_
-     * \return The current value of init_value_
-     */
-    DataContainer<double> InitValue() const noexcept { return init_value_; }
-    /** Set init_value_
-     * \param val New value to set
-     */
-    void InitValue(const DataContainer<double>& val) noexcept { init_value_ = val; }
-    /** Access block_size_
-     * \return The current value of block_size_
-     */
-    DataContainer<double> TrueSol() const noexcept { return true_sol_; }
-    /** Set true_sol_
-     * \param val New value to set
-     */
-    void TrueSol(const DataContainer<double>& val) noexcept { true_sol_ = val; }
-
-protected:
-
-private:
-    float tol_ = 1e-4; //!< Member variable "tol_"
-    size_t max_iter_ = 2000; //!< Member variable "max_iter_"
-    DataContainer<double> init_value_; //!< Member variable "init_value_"
-    DataContainer<double> true_sol_; //!< Member variable "true_sol_"
-    bool log_ = true; //!< Member variable "log_"
-    unsigned int log_period_ = 10; //!< Member variable "log_period_"
+    double tol; //!< Member variable "tol"
+    size_t max_iter = 2000; //!< Member variable "max_iter"
+    DataContainer<double> init_value; //!< Member variable "init_value"
+    bool log; //!< Member variable "log"
+    unsigned int log_period; //!< Member variable "log_period"
 };
 } // namespace poisson
 } // namespace fista
