77fb5a5153280c0e9ea639022f549e5b2a6058f0
Philippe Ganz <philippe.ganz@gmail.com>
2018-01-04 21:13:00 +0100

    Change the Operator class
    * Remove fun_, operator behavior will be done through derived classes
    * Add a Matrix for the operator's data and a bool to set the transpose state

--- include/utils/operator.hpp
+++ include/utils/operator.hpp
@@ -2,9 +2,9 @@
 /// \file include/utils/operator.hpp
 /// \brief Operator class header
 /// \details Provide operator container with operator-vector operations
-/// \author Philippe Ganz <philippe.ganz@gmail.com> 2017
+/// \author Philippe Ganz <philippe.ganz@gmail.com> 2017-2018
 /// \version 0.2.0
-/// \date 2017-12-28
+/// \date 2018-01-04
 /// \copyright GPL-3.0
 ///
 
@@ -21,73 +21,180 @@ namespace astroqut
 template<class T>
 class Operator : public LinearOp<T>
 {
-private:
-    std::function<Matrix<T> (Matrix<T>&, Matrix<T>&)> fun_;
+protected:
+    Matrix<T> data_; //!< Member variable "data_"
+    bool transposed_; //!< Member variable "transposed_"
 
 public:
     /** Default constructor
      */
     Operator()
-    {}
-
-    /** Default destructor
-     */
-    virtual ~Operator()
+        : data_()
+        , transposed_(false)
     {}
 
     /** Copy constructor
      *  \param other Object to copy from
      */
     Operator(const Operator& other)
-        : LinearOp<T>(other.Height(), other.Width())
-        , fun_(other.Fun())
+        : LinearOp<T>(other.height_, other.width_)
+        , data_(other.data_)
+        , transposed_(other.transposed_)
     {}
 
-    /** Full member constructor
-     *  \param fun Function describing the matrix-like operator
+    /** Empty constructor
      *  \param height Height of the operator
      *  \param width Width of the operator
      */
-    Operator(std::function<Matrix<T> (Matrix<T>&, Matrix<T>&)> fun, const size_t height, const size_t width) noexcept
+    Operator(size_t height, size_t width) noexcept
         : LinearOp<T>(height, width)
-        , fun_(fun)
+        , data_(nullptr, 0, 0)
+        , transposed_(false)
     {}
 
-    /** Access fun_
-     * \return The current value of fun_
+    /** Full member constructor
+     *  \param data Array containing data used by the operator
+     *  \param height Height of the operator
+     *  \param width Width of the operator
+     *  \param transposed If the operator is transposed
      */
-    std::function<Matrix<T> (Matrix<T>&, Matrix<T>&)> Fun() const noexcept
-    {
-        return fun_;
-    }
-    /** Set fun_
-     * \param val New value to set
+    Operator(const Matrix<T>& data, size_t height, size_t width, bool transposed) noexcept
+        : LinearOp<T>(height, width)
+        , data_(data)
+        , transposed_(transposed)
+    {}
+
+    /** Clone function
+     *  \return A copy of the current instance
      */
-    void Fun(std::function<Matrix<T> (Matrix<T>&, Matrix<T>&)> fun) noexcept
+    virtual Operator* Clone() const
     {
-        fun_ = fun;
+        return new Operator(*this);
     }
 
-//    Matrix<T> operator*(const Matrix<T>& other) const override final
-//    {
-//        Matrix<T> result()
-//
-//        if( ArgTest(other, mult) )
-//        {
-//            try
-//            {
-//                result_data = new T[this->height_*other.width_] {}; // init to zero
-//            }
-//            catch (const std::bad_alloc& ba)
-//            {
-//                std::cerr << "Could not allocate memory for resulting array!" << std::endl;
-//                throw;
-//            }
-//
-//        }
-//        return fun_(other, result_data);
-//    }
+    /** Default destructor
+     */
+    virtual ~Operator()
+    {}
+
+    /** Access data_
+     * \return The current value of data_
+     */
+    Matrix<T> Data() const noexcept
+    {
+        return data_;
+    }
+    /** Set data_
+     * \param data New value to set
+     */
+    void Data(const Matrix<T>& data) noexcept
+    {
+        data_ = data;
+    }
+
+    /** Valid instance test
+     *  \return Throws an error message if instance is not valid.
+     */
+    virtual bool IsValid() const override
+    {
+        if( this->height_ != 0 &&
+            this->width_ != 0 )
+        {
+            return true;
+        }
+        else
+        {
+            throw std::invalid_argument("Operator dimensions must be non-zero and function shall not be nullptr!");
+        }
+    }
+
+    /** Access transposed_
+     * \return The current value of transposed_
+     */
+    bool IsTransposed() const noexcept
+    {
+        return transposed_;
+    }
+
+    /** Comparison operator equal
+     *  \param other Object to compare to
+     *  \return True if both object are the same element-wise, False else
+     */
+    bool operator==(const Operator& other) const noexcept
+    {
+        if( this->height_ != other.height_ ||
+            this->width_ != other.width_ )
+        {
+            return false;
+        }
+
+        if( data_ == other.data_ )
+        {
+            return true;
+        }
+    }
+
+    /** Comparison operator not-equal
+     *  \param other Object to compare to
+     *  \return False if both object are the same element-wise, True else
+     */
+    bool operator!=(const Operator& other) const noexcept
+    {
+        return !(*this == other);
+    }
+
+    /** Assignment operator
+     *  \param other Object to assign to current object
+     *  \return A reference to this
+     */
+    Operator& operator=(const Operator& other)
+    {
+        if( this != &other )
+        {
+            this->height_ = other.height_;
+            this->width_ = other.width_;
+            this->length_ = other.length_;
+            data_ = other.data_;
+            transposed_ = other.transposed_;
+        }
+        return *this;
+    }
+
+    /** Move operator
+     *  \param other Object to move to current object
+     *  \return A reference to this
+     */
+    Operator& operator=(Operator&& other) noexcept
+    {
+        if( this != &other )
+        {
+            this->height_ = other.height_;
+            this->width_ = other.width_;
+            this->length_ = other.length_;
+            std::swap(data_, other.data_);
+            transposed_ = other.transposed_;
+        }
+        return *this;
+    }
+
+    virtual Matrix<T> operator*(const Matrix<T>& other) const
+    {
+        std::cerr << "Do not use the Operator class directly, please use one of its derived classes." << std::endl;
+
+        return Matrix(other);
+    }
+
+    /** Transpose in-place
+     *   \return A reference to this
+     */
+    virtual Operator& Transpose()
+    {
+        std::swap(this->height_, this->width_);
+        transposed_ = !transposed_;
+        return *this;
+    }
 };
+
 } // namespace astroqut
 
 #endif // ASTROQUT_UTILS_OPERATOR_HPP
