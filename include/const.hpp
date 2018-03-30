eded599b508c62415dc57caaa3d50bfdd8d99ad0
Philippe Ganz <philippe.ganz@gmail.com>
2018-03-30 19:34:18 +0200

    Add new flags for development phase

--- include/const.hpp
+++ include/const.hpp
@@ -2,14 +2,16 @@
 /// \file include/const.hpp
 /// \brief Constant values used throughout the whole project
 /// \author Philippe Ganz <philippe.ganz@gmail.com>
-/// \version 0.2.0
-/// \date 2018-01-07
+/// \version 0.3.0
+/// \date 2018-03-30
 /// \copyright GPL-3.0
 ///
 
 #ifndef ASTROQUT_CONST_HPP
 #define ASTROQUT_CONST_HPP
 
-//#define DEBUG
+#define DEBUG
+#define VERBOSE
+#define DO_ARGCHECKS
 
 #endif // ASTROQUT_CONST_HPP
