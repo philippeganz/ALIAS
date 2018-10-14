cd945dc6a3b3fc0d4bf36f3124192a475c0827c6
Philippe Ganz <philippe.ganz@gmail.com>
2018-10-14 20:22:04 +0200

    Update launcher

--- src/main.cpp
+++ src/main.cpp
@@ -3,8 +3,8 @@
 /// \brief Launcher for the ASTROQUT solver.
 /// \details Handle the user input, calls the preparation tools and the solver.
 /// \author Philippe Ganz <philippe.ganz@gmail.com>
-/// \version 0.5.0
-/// \date 2018-07-07
+/// \version 0.6.0
+/// \date 2018-10-14
 /// \copyright GPL-3.0
 ///
 
@@ -12,7 +12,6 @@
 #include "test.hpp"
 
 #include <cstdlib>
-#include <fstream>
 #include <iostream>
 #include <string>
 
@@ -23,17 +22,6 @@ int main( int argc, char **argv )
     try
     {
         astroqut::test::MatrixTest<double>();
-    }
-    catch (const std::exception& err)
-    {
-        std::cerr << err.what() << std::endl;
-        std::cerr << "Matrix tests failed! Please refer to the individual test results for more details." << std::endl;
-        return EXIT_FAILURE;
-    }
-
-    try
-    {
-        astroqut::test::PerfTest<double>(2048);
     }
     catch (const std::exception& err)
     {
@@ -64,8 +52,8 @@ int main( int argc, char **argv )
         return EXIT_FAILURE;
     }
 
-    astroqut::test::astro::Chandra();
-
+//    astroqut::test::astro::Chandra();
+    astroqut::test::astro::Sym256();
 
 //    if( argc != 6 )
 //    {
@@ -73,7 +61,9 @@ int main( int argc, char **argv )
 //        std::cerr << "  source - Path to the source image;" << std::endl;
 //        std::cerr << "  sensitivity - Path to the sensitivity image or an integer >= 1 in which case we consider constant sensitivity;" << std::endl;
 //        std::cerr << "  background - Path to the background image or an integer >= 0 in which case we consider constant background;" << std::endl;
+//        std::cerr << "  solution - Path to the solution file;" << std::endl;
 //        std::cerr << "  size - Width of the picture;" << std::endl;
+//        std::cerr << "  bootstrap - Width of the picture;" << std::endl;
 //        std::cerr << "  option file - Path to the parameters file." << std::endl << std::endl;
 //        return EXIT_FAILURE;
 //    }
